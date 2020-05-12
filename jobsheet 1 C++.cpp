22066f3bfdaf72207366cb6139fbd294419a87d3
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 21:19:08 +0700

    Create jobsheet 1 C++.cpp
    membuat program array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+	int nim[7]= {1,2,3,4,5,0,5};
+	cout<<"nama:aditya sari caessar\n";
+	cout<<"nim:F1B019007\n";
+	cout<<"kelompok:3 \n";
+	cout<<("\ndata ke-1= ",nim[0]);
+	cout<<("\ndata ke-2= ",nim[1]);
+	cout<<("\ndata ke-3= ",nim[2]);
+	cout<<("\ndata ke-4= ",nim[3]);
+	cout<<("\ndata ke-5= ",nim[4]);
+	cout<<("\ndata ke-6= ",nim[5]);
+	cout<<("\ndata ke-7= ",nim[6]);
+	cout<<("\ndata ke-8= ",nim[7]);
+	cout<<("\ndata ke-9= ",nim[8]);
+
+}
