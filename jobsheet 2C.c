d859b4d5ec4ebb885fad29b72795f64d4b10919f
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 20:43:52 +0700

    Create jobsheet 2C.c
    membuat program mengisi dan menampilkan elemen array denan index array sesuai dengan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 2C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+main(){
+	int x[7];
+	printf("Masukkan nilai ke-1:");
+	scanf("%i",&x[0]);
+	printf("Masukkan nilai ke-2:");
+	scanf("%i",&x[1]);
+	printf("Masukkan nilai ke-3:");
+	scanf("%i",&x[2]);
+	printf("Masukkan nilai ke-4:");
+	scanf("%i",&x[3]);
+	printf("Masukkan nilai ke-5:");
+	scanf("%i",&x[4]);
+	printf("Masukkan nilai ke-6:");
+	scanf("%i",&x[5]);
+	printf("Masukkan nilai ke-7:");
+	scanf("%i",&x[6]);
+	printf("\n");
+	printf("nilai ke-1= %i\n",x[0]);
+	printf("nilai ke-2= %i\n",x[1]);
+	printf("nilai ke-3= %i\n",x[2]);
+	printf("nilai ke-4= %i\n",x[3]);
+	printf("nilai ke-5= %i\n",x[4]);
+	printf("nilai ke-6= %i\n",x[5]);
+	printf("nilai ke-7= %i\n",x[6]);
+	return 0;
+}
