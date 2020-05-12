945d3d9c45288b6d74ea9a12d86e5d28fe490680
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 21:36:57 +0700

    Create jobsheet 8 C++.cpp
     membuat program mengisi nilai array dan menampilkanya dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 8 C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x[7],i;
+	for(i=0;i<=6;i++){
+	cout<<"Masukkan nilai :"<<i+1<<":";
+	cin>>x[i];}
+	cout<<endl;
+	for(i=0;i<=6;i++){
+	cout<<"nilai ke "<<i+1<<"= "<<x[i]<<endl;
+	}
