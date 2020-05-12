5e5ba503e0a613683beea6bb25b5bd9af5e6f2f9
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 21:20:56 +0700

    Create jobsheet 2 C++.cpp
     membuat program mengisi dan menampilkan elemen array denan index array sesuai dengan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+main(){
+	int x[7];
+	cout<<"nama:aditya sari caessar\n";
+	cout<<"nim:F1B019007\n";
+	cout<<"kelompok:3 \n";
+	cout<<"Masukkan nilai ke-1:";
+	cin>>x[0];
+	cout<<"Masukkan nilai ke-2:";
+	cin>>x[1];
+	cout<<"Masukkan nilai ke-3:";
+	cin>>x[2];
+	cout<<"Masukkan nilai ke-4:";
+	cin>>x[3];
+	cout<<"Masukkan nilai ke-5:";
+	cin>>x[4];
+	cout<<"Masukkan nilai ke-6:";
+	cin>>x[5];
+	cout<<"Masukkan nilai ke-7:";
+	cin>>x[6];
+	cout<<endl;
+	cout<<"nilai  ke-1= "<<x[0]<<endl;
+	cout<<"nilai  ke-2= "<<x[1]<<endl;
+	cout<<"nilai  ke-3= "<<x[2]<<endl;
+	cout<<"nilai  ke-4= "<<x[3]<<endl;
+	cout<<"nilai  ke-5= "<<x[4]<<endl;
+	cout<<"nilai  ke-6= "<<x[5]<<endl;
+	cout<<"nilai  ke-7= "<<x[6]<<endl;
+	system("PAUSE");
+}
+
