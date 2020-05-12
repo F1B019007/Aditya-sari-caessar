c2d8710f5c7bd9de464c06f1c005bc688ab8af9e
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 21:38:31 +0700

    Create jobsheet 9 C++.cpp
     membuat program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ jobsheet 9 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int i;
+char nama[10]={'d','i','t','h','a','\0'};
+char nama2[10]="ditha";
+cout<<"array per karakter =";
+for(i-0;i<=5;i++){
+cout<<nama[i];
+}
+cout<<endl;
+cout<<"array string = "<<nama2<<endl;
+}
