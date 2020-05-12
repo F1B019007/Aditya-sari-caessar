a4bcfc136a2a4c3c7e3246be6dd0cbd4abeece22
Aditya sari caessar <65231545+F1B019007@users.noreply.github.com>
2020-05-12 20:39:02 +0700

    Create jobsheet 1C.c
     Membuat program array satu dimensi yang menampilkan NIM masing-masing Mahasiswa

---
+++ jobsheet 1C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+main(){
+	int nim[7]= {1,2,3,4,5,0,5};
+	printf("nama:aditya sari caessar");
+	printf("\nnim: F1B019007");
+	printf("\nkelompok: 3\n");
+	
+	printf("\ndata ke-1= %d",nim[0]);
+	printf("\ndata ke-2= %d",nim[1]);
+	printf("\ndata ke-3= %d",nim[2]);
+	printf("\ndata ke-4= %d",nim[3]);
+	printf("\ndata ke-5= %d",nim[4]);
+	printf("\ndata ke-6= %d",nim[5]);
+	printf("\ndata ke-7= %d",nim[6]);
+	printf("\ndata ke-8= %d",nim[7]);
+	printf("\ndata ke-9= %d",nim[8]);
+
+
+}
